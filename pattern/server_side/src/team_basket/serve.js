9fd24ebfe52aaad558baa6f85b0c394f6e125fcf
shibamoto <silverbirder@gmail.com>
2021-04-04 10:56:58 +0900

    🙂 can do communication to inspire from basket

--- pattern/server_side/src/team_basket/serve.js
+++ pattern/server_side/src/team_basket/serve.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Team Basket')
-})
+app.get('/',  async (req, res) => {
+  const inspire = await (await fetch('http://team-inspire-app:4000')).text();
+  res.send(`Team Basket. inspire ${inspire}`);
+});
 
 app.listen(port, () => {
   console.log(`Team Basket app listening at http://localhost:${port}`)
-})
+});
