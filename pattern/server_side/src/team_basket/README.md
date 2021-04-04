e58f81ee7dd37c93901e5f618c656ab2692500a0
shibamoto <silverbirder@gmail.com>
2021-04-04 12:04:45 +0900

    🙂 shift inspire host variable from code to yml

--- pattern/server_side/src/team_basket/README.md
+++ pattern/server_side/src/team_basket/README.md
@@ -4,3 +4,9 @@
 ```
 $ docker build -t team-basket-app:v1 .
 ```
+
+## Minikube
+### Service
+```
+$ minikube service team-basket-app
+```
--- pattern/server_side/src/team_basket/serve.js
+++ pattern/server_side/src/team_basket/serve.js
@@ -2,9 +2,10 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 3000;
+const inspireHost = process.env.INSPIRE_HOST || 'team-inspire-app:4000';
 
 app.get('/',  async (req, res) => {
-  const inspire = await (await fetch('http://team-inspire-app:4000')).text();
+  const inspire = await (await fetch(`http://${inspireHost}`)).text();
   res.send(`Team Basket. inspire ${inspire}`);
 });
 
