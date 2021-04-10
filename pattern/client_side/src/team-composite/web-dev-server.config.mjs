47be14c59d41a17c672b52794a20685adc87712c
shibamoto <silverbirder@gmail.com>
2021-04-10 15:53:25 +0900

    🙂 run composite proxy routing on skaffold

--- pattern/client_side/src/team-composite/web-dev-server.config.mjs
+++ pattern/client_side/src/team-composite/web-dev-server.config.mjs
@@ -1,8 +1,15 @@
 // import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+import fetch from 'node-fetch';
 
 /** Use Hot Module replacement by adding --hmr to the start command */
 const hmr = process.argv.includes('--hmr');
 
+const proxyMap = {
+  'basket': 'http://team-basket-text:3000',
+  'inspire': 'http://team-inspire-text:4000',
+  'product': 'http://team-product-text:5000',
+  'search': 'http://team-search-text:7000',
+};
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: true,
   open: false,
@@ -26,4 +33,31 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
 
   // See documentation for all available options
   port: 2000,
+  middleware: [
+    async function rewriteIndex(context, next) {
+      async function _proxyFetch(url, context) {
+        const response = await fetch(url);
+        const text = await response.text();
+        context.res.writeHead(response.status, { 'Content-Type': response.headers.get('content-type')});
+        context.res.write(text);
+        context.res.end();
+      }
+      const url = context.request.url;
+      switch (true) {
+        case /^\/basket\//.test(url):
+          await _proxyFetch(`${proxyMap['basket']}/${url.split('/basket/')[1]}`, context);
+          break;
+        case /^\/inspire\//.test(url):
+          await _proxyFetch(`${proxyMap['inspire']}/${url.split('/inspire/')[1]}`, context);
+          break;
+        case /^\/product\//.test(url):
+          await _proxyFetch(`${proxyMap['product']}/${url.split('/product/')[1]}`, context);
+          break;
+        case /^\/search\//.test(url):
+          await _proxyFetch(`${proxyMap['search']}/${url.split('/search/')[1]}`, context);
+          break;
+      }
+      return next();
+    },
+  ],
 });
